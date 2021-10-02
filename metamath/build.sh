7d06cbb3477f766a40f7d81e21557b79b377b77e
Julius Michaelis <gitter@liftm.de>
2021-10-02 17:09:10 +0900

    metamath

---
+++ metamath/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -eu
+
+podman build -t metamath-wasm-build -<Dockerfile
+cont=$(podman create metamath-wasm-build)
+trap "podman rm $cont" EXIT
+podman cp $cont:/work/metamath/metamath.wasm .
