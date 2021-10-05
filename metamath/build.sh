e8f027bbc3651d5b0649c1c9827bcde3adc023f1
Julius Michaelis <glitter@liftm.de>
2021-10-06 08:14:27 +0900

    metamath: wasi?

--- metamath/build.sh
+++ metamath/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-podman build -t metamath-wasm-build -<Dockerfile
+podman build -t metamath-wasm-build . 
 cont=$(podman create metamath-wasm-build)
 trap "podman rm $cont" EXIT
 podman cp $cont:/work/metamath/metamath.wasm .
